7ddb95829a2c0f646a14a677f2aeed09996342fc
Rob Sanchez <rsanchez@barrettnewton.com>
2011-10-16 07:43:13 -0500

    fixed button not showing on latest github

--- github_bookmarks.user.js
+++ github_bookmarks.user.js
@@ -77,7 +77,7 @@ var githubBookmarks = {
 				}
 			});
 			
-			$(".repohead .title-actions-bar ul.actions").prepend(button);
+			$(".repohead .title-actions-bar ul.pagehead-actions").prepend(button);
 		}
 		
 		/* ADD BOOKMARKED REPOS LIST */
